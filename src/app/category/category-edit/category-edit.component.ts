63a13deaa54eb50a9bfe89a26f39d9e4a84f0335
PhuongNamHoang198 <hoangphuongnam.98@gmail.com>
2022-10-26 06:39:15 +0700

    add category-edit

--- src/app/category/category-edit/category-edit.component.ts
+++ src/app/category/category-edit/category-edit.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from "@angular/core";
+import {FormGroup} from "@angular/forms";
+import {CategoryService} from "../../service/category.service";
+import {ActivatedRoute, ParamMap} from "@angular/router";
 
 @Component({
   selector: 'app-category-edit',
@@ -6,10 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./category-edit.component.css']
 })
 export class CategoryEditComponent implements OnInit {
+  categoryForm: FormGroup;
+  id: number;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private categoryService: CategoryService,
+              private activatedRoute: ActivatedRoute) {
+    this.activatedRoute.paramMap.subscribe((paramMap: ParamMap) => {
+      this.id = +paramMap.get('id');
+      this.getCategory(this.id);
+    });
   }
 
+  ngOnInit() {
+  }
+
+  getCategory(id: number) {
+    return this.categoryService.findById(id).subscribe(category => {
+      this.categoryForm = new FormGroup({
+        name: new FormControl(category.name),
+      });
+    });
+  }
+
+  updateCategory(id: number) {
+    const category = this.categoryForm.value;
+    this.categoryService.updateCategory(id, category).subscribe(() => {
+      alert('Cập nhật thành công');
+    }, e => {
+      console.log(e);
+    });
+  }
 }
